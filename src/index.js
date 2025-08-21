5bc5e8f769df30c63f99bc89770d73b132fac940
RstK <208619198+bowman025@users.noreply.github.com>
2025-08-21 18:40:25 +0200

    Edit how index.js functions slightly and style text on bottom of home

--- src/index.js
+++ src/index.js
@@ -1,41 +1,30 @@
-import {greetingHome} from "./tabs/home.js";
-import {greetingMenu} from "./tabs/menu.js";
-import {greetingAbout} from "./tabs/about.js";
-import "./styles.css";
-
-console.log("Hello! I am alive!");
-console.log(greetingHome);
-console.log(greetingMenu);
-console.log(greetingAbout);
-
 import { createMenu } from "./tabs/menu.js";
 import { createAbout } from "./tabs/about.js";
 import { createHome } from "./tabs/home.js";
+import "./styles.css";
 
-const content = document.querySelector("#content");
-
-const homeBtn = document.querySelector(".home-btn");
-homeBtn.addEventListener("click", () => {
-    removeContent();
-    createHome();
-});
-
-const menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click", () => {
-    removeContent();
-    createMenu();
-});
-
-const aboutBtn = document.querySelector(".about-btn");
-aboutBtn.addEventListener("click", () => {
-    removeContent();
-    createAbout();
-});
-
-function removeContent() {
-    while (content.firstChild) {
-        content.removeChild(content.firstChild);
+function createPage() {
+    const content = document.querySelector("#content");
+    const homeBtn = document.querySelector(".home-btn");
+    homeBtn.addEventListener("click", () => {
+        removeContent();
+        createHome();
+    });
+    const menuBtn = document.querySelector(".menu-btn");
+    menuBtn.addEventListener("click", () => {
+        removeContent();
+        createMenu();
+    });
+    const aboutBtn = document.querySelector(".about-btn");
+    aboutBtn.addEventListener("click", () => {
+        removeContent();
+        createAbout();
+    });
+    function removeContent() {
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        };
     };
+    createHome();
 };
-
-createHome();
+createPage();
